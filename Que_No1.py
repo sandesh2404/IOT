e0d89ae0069c624ac2150f1b5d5ee7d27c98263d
sandesh2404 <sandeshgpp.com>
2024-04-16 14:52:30 +0530

    Day01 Que no1 completed

---
+++ Que_No1.py
@@ -0,0 +1,12 @@
+# find area and perimeter of rectangle
+
+le = float(input("Enter length of rectangle : "))
+br = float(input("Enter breadth of rectangle : "))
+
+area = le * br
+peri = 2 * (le + br)
+
+print(f"Area of rectangle = {area}")
+print(f"Perimeter of rectangle = {peri}")
+
+
